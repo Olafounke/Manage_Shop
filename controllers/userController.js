8cd0d84b1adf1032ec272d7331abf9daf09408e3
Romain Segarizzi <romain.segarizzi@hotmail.fr>
2024-11-13 16:54:38 +0100

    Ajout du fingerprint + autre élement dans le payload

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,24 +5,37 @@ const dotenv = require("dotenv").config().parsed;
 
 async function mineToken(prefix, hash) {
   let nonce = 0;
-
   console.log("hash :", hash);
+
+  // Boucle qui permet de definir le nonce et le nouveau hash
   while (!hash.startsWith(prefix)) {
     const data = `${hash}.${nonce}`;
     hash = crypto.createHash("sha256").update(data).digest("hex");
     nonce++;
   }
-
   return { nonce, hash };
 }
 
-// Fonction pour créer un token expirable
-function createToken(userId, secret) {
+function generateDeviceFingerprint(req) {
+  const userAgent = req.headers["user-agent"] || " ";
+  const ip = req.ip || " ";
+  const timezone = req.headers["timezone"] || " ";
+  const fingerprintData = `${userAgent}-${ip}-${timezone}`;
+  return crypto.createHash("sha256").update(fingerprintData).digest("hex");
+}
+
+function createToken(userId, secret, req) {
   // Défini le payload avec userId et l'expiration dans 24 heures
   const payload = {
     userId: userId,
+    role: "user",
+    issuedAt: Date.now(),
+    scope: ["read", "write"],
+    issuer: "authServer",
+    deviceFingerprint: generateDeviceFingerprint(req),
     exp: Math.floor(Date.now() / 1000) + 24 * 60 * 60,
   };
+  console.log(payload);
   const encodedPayload = Buffer.from(JSON.stringify(payload)).toString(
     "base64"
   );
@@ -75,8 +88,11 @@ exports.login = (req, res) => {
           }
 
           // Création du token
-          const userId = 123;
-          const token = createToken(userId, dotenv.SECRET_KEY);
+          const token = createToken(
+            user._id.toString(),
+            dotenv.SECRET_KEY,
+            req
+          );
 
           // Minage du token
           const result = mineToken("000", token).then((result) => {
