74e1d79ae3845479244c0660c2bd4b48c63ce48a
Romain Segarizzi <romain.segarizzi@hotmail.fr>
2025-01-05 23:05:18 +0100

    Modification de la creation du token + vérification du proofOfWork

--- middelware/token.js
+++ middelware/token.js
@@ -1,37 +1,38 @@
 const crypto = require("crypto");
 
-function generateDeviceFingerprint(req) {
-  const userAgent = req.headers["user-agent"] || " ";
-  const ip = req.ip || " ";
-  const timezone = req.headers["timezone"] || " ";
-  const fingerprintData = `${userAgent}-${ip}-${timezone}`;
-  return crypto.createHash("sha256").update(fingerprintData).digest("hex");
-}
-
-function createToken(userId, secret, req) {
-  // Défini le payload avec userId et l'expiration dans 24 heures
+async function createToken(userId, secret, req) {
   const payload = {
     userId: userId,
     role: "user",
     issuedAt: Date.now(),
+    expiresIn: Date.now() + 24 * 60 * 60 * 1000,
+    nonce: 0,
+    proofOfWork: "",
     scope: ["read", "write"],
     issuer: "authServer",
     deviceFingerprint: generateDeviceFingerprint(req),
-    exp: Math.floor(Date.now() / 1000) + 24 * 60 * 60,
   };
-  console.log(payload);
-  const encodedPayload = Buffer.from(JSON.stringify(payload)).toString(
-    "base64"
-  );
 
-  // Génére la signature avec HMAC-SHA256
+  // Encodage initial du payload
+  const preEncodedPayload = encodeToBase64(payload);
+
+  // Minage du nonce et mise à jour dans le payload
+  const { nonce, hash } = await mineNonce(preEncodedPayload);
+  payload.nonce = nonce;
+  payload.proofOfWork = hash;
+
+  // Encodage du payload
+  const encodedPayload = encodeToBase64(payload);
+
+  // Génération de la signature
   const signature = crypto
     .createHmac("sha256", secret)
     .update(encodedPayload)
     .digest("base64");
+
+  // Encodage de la signature
   const encodedSignature = Buffer.from(signature).toString("base64");
 
-  // Assemble le token complet
   return `${encodedPayload}.${encodedSignature}`;
 }
 
@@ -50,10 +51,19 @@ function verifyToken(token, secret) {
     return null;
   }
 
-  // Vérifie si le token est expiré
+  // Decodage du payload
   const payload = JSON.parse(Buffer.from(encodedPayload, "base64").toString());
-  if (payload.exp < Math.floor(Date.now() / 1000)) {
+
+  // Vérifie si le token est expiré
+  if (payload.expiresIn < Date.now()) {
     console.log("Le token a expiré. L'utilisateur doit se reconnecter.");
+    return null;
+  }
+
+  // Vérifie si la proofOfWork est correct
+  const { proofOfWork, proofOfWorkCheck } = verifyNonce(payload);
+  if (proofOfWork !== proofOfWorkCheck) {
+    console.log("Le token est invalide");
     return null;
   }
 
@@ -62,9 +72,21 @@ function verifyToken(token, secret) {
   return payload;
 }
 
-async function mineToken(prefix, hash) {
+function generateDeviceFingerprint(req) {
+  const userAgent = req.headers["user-agent"] || " ";
+  const ip = req.ip || " ";
+  const timezone = req.headers["timezone"] || " ";
+  const fingerprintData = `${userAgent}-${ip}-${timezone}`;
+  return crypto.createHash("sha256").update(fingerprintData).digest("hex");
+}
+
+function encodeToBase64(params) {
+  return Buffer.from(JSON.stringify(params)).toString("base64");
+}
+
+async function mineNonce(hash) {
   let nonce = 0;
-  console.log("hash :", hash);
+  const prefix = "000";
 
   // Boucle qui permet de definir le nonce et le nouveau hash
   while (!hash.startsWith(prefix)) {
@@ -72,7 +94,27 @@ async function mineToken(prefix, hash) {
     hash = crypto.createHash("sha256").update(data).digest("hex");
     nonce++;
   }
+
   return { nonce, hash };
 }
 
-module.exports = { createToken, verifyToken, mineToken };
+function verifyNonce(payload) {
+  const nonce = payload.nonce;
+  const proofOfWork = payload.proofOfWork;
+
+  // Pre-encodage du payload pour retrouver la proofOfWork
+  payload.nonce = 0;
+  payload.proofOfWork = "";
+  const preEncodedPayload = encodeToBase64(payload);
+
+  // Boucle avec le nonce qui permet de verifier la proofOfWork
+  let proofOfWorkCheck = preEncodedPayload;
+  for (let currentNonce = 0; currentNonce < nonce; currentNonce++) {
+    const data = `${proofOfWorkCheck}.${currentNonce}`;
+    proofOfWorkCheck = crypto.createHash("sha256").update(data).digest("hex");
+  }
+
+  return { proofOfWork, proofOfWorkCheck };
+}
+
+module.exports = { createToken, verifyToken };
