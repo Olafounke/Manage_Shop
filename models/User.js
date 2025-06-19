ec93ce7e4e897125591588e6ca5b4aaf9fb80418
Romain Segarizzi <romain.segarizzi@hotmail.fr>
2025-06-19 11:58:59 +0200

    Restructuration complet de l'API Gateway

--- models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-//Modèle "User"
-const userSchema = mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-});
-
-module.exports = mongoose.model("User", userSchema);
